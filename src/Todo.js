a9be67fe7efb6ba3d2283277eac269f595895db1
Milena <lenamsst@gmail.com>
2022-04-30 23:09:37 -0300

    feat: componetizando o form

--- src/Todo.js
+++ src/Todo.js
@@ -1,38 +1,29 @@
 import React, {useState} from 'react'
 import './Todo.css'
+import List from './components/List'
+import TodoForm from './components/TodoForm'
 
 
 function Todo() {
-    const [text, setText] = useState("");
+
     const [items, setItems] = useState([]);
 
-    function handleChange(event) {
-        let t = event.target.value;
-        setText(t);
+    function onAddItem(item){
+        setItems([...items, item])
     }
-
-    function addItem(event){
-        event.preventDefault();
-        if (text) {
-            setItems([...items, text])
-            setText("");
-        }
-    }
+ 
 
     return (<div className="container">
         <h1>Todo</h1>
-        <form>
-            <input onChange={handleChange} type="text" value={text}></input>
-            <button onClick={addItem}>Add</button>
-        </form>
-
-        <ul>
-            {items.map(item => <li>{item}</li>)}
-        </ul>
+       
+        <TodoForm onAddItem={onAddItem}></TodoForm>
+        <List items={items}></List>
 
         </div>)
 
 }
+
+
 
 
 export default Todo
